7a8d366083f5f0e1735b6c3d8c3dedc5be9753e3
suzy009 <94984306+suzy009@users.noreply.github.com>
2021-11-24 11:41:19 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Milestone 4: Status Update
+
+Tell me what you've gotten done so far, what obstacles you have encountered, and whether / how your plans have changed as you have gotten to work. The writing style can be more informal than the proposal and the final reports. I want you to spend time on the project not the writing -- my goal is to make sure you've all gotten a good start instead of putting things off until the last minute, and to give you feedback if I see any signs of danger.
+
+### Document Format:
+Your update should be no more than 250-500 words and include the following items. This update should be uploaded to this repo as a separate PDF, including:
+
+- images of the current interface (rough prototype is fine). These do not need to be beautiful, just show the progress you've made in getting to a working interface.
+- any modifications to your proposed implementation approach. What libraries have you been using? Has your implementation approach changed significantly? If so, why?
+- milestones and schedule. How is your project tracking against your milestones? Are you on schedule? If not, what's blocking you from making progress?
+- your code pushed to this repo, with a public-facing link to the interface. It is recommended that you host the page directly through GitHub, which should be sufficient if all you need is a static site (i.e., no database or server-side code).
+
+### Note:
+If you did not receive full credit for your design document, particularly the project realization, this is an opportunity to show that you have responded to the comments during the poster session.
+
+## Grading: 
+- [10%] Screenshots of the current interface status
+- [15%] Description of changes to the previous proposed approach
+- [20%] Description of implementation (libraries, etc)
+- [10%] Description of current schedule 
+- [25%] Current challenges
+- [20%] Current code
+
+### Formatting: 
+For all assignments involving written reports, use Times Roman 11 point type with normal margins, 1.5 line spacing. 
---
+++ js/index.js
@@ -0,0 +1,217 @@
+var whole_w = 1024;
+var whole_h = 768;
+
+var image_w = 200;
+var image_h = 200;
+
+var active = d3.select(null);
+
+var projection = d3.geo.albersUsa()
+    .translate([whole_w/3, whole_h/3.5])
+    .scale([900]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var Opacity = d3.scale.linear()
+    .range([0.2, 0.9]);
+
+var Scale = d3.scale.linear()
+    .range([2, 20]);
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", whole_w)
+    .attr("height", whole_h);
+
+var g = svg.append("g")
+    .attr("class","map");
+
+
+d3.csv("data/US-states.csv", function(data) {
+    d3.json("data/US-geo.json", function(json) {
+        for (var i = 0; i < data.length; i++) {
+            var dataState = data[i].state;				
+            var dataEASTorWEST = data[i].EASTorWEST;
+            for (var j = 0; j < json.features.length; j++) {
+                var jsonState = json.features[j].properties.name;
+                if (dataState == jsonState) {
+                    json.features[j].properties.EASTorWEST = dataEASTorWEST;
+                    break;
+                }
+            }
+        }
+
+        g.selectAll("path")
+            .data(json.features).enter()
+            .append("path")
+            .attr("stroke","white")
+            .attr("stroke-width",2)
+            .attr("d", path)
+            .attr("class", function(d) {
+                return d.properties.postal;})
+            .style("fill", function(d) {
+                var EASTorWEST = d.properties.EASTorWEST;
+
+                if (EASTorWEST) {
+                    if (EASTorWEST == "East") {
+                        return "#7abbff";
+                    } else {
+                        return "#ff9493";
+                    }
+                } else {
+                    return "#CCCCCC";
+                }
+            })
+            .on("click", click_on_state);
+
+        d3.csv("data/team_info.csv", function(data) {
+            Scale.domain([0, d3.max(data, function(d) { return parseInt(d.winrate*100) })]);
+            Opacity.domain([0, d3.max(data, function(d) { return parseInt(d.winrate*100) })]);
+            var font_Size = d3.scale.linear()
+            var FontSize = d3.scale.linear()
+                .domain([15, 1])
+                .range([10, 20]);
+
+            var nodes = g.selectAll("nodes")
+                .data(data)
+                .enter()
+                .append("g")
+                .attr("class", "team")
+                .attr("transform", function(d) {
+                    return "translate(" + projection([d.lon, d.lat])[0] + "," + projection([d.lon, d.lat])[1] + ")";})
+                .on("mouseover", hover_on_node)
+                .on("mouseout", hover_off_node);
+
+            nodes.append("circle")
+                .attr("class", function(d) { return d.abbr })
+                .attr("r", function(d){
+                    return Scale(parseInt(d.winrate*100));})
+                .style("fill", function(d){
+                    if (d.EASTorWEST == "East") {
+                        return "blue";
+                    } else {
+                        return "red";
+                    };
+                })
+                .style("opacity", function(d){
+                    return Opacity(parseInt(d.winrate*100));})
+                .style("cursor", "pointer")
+                .on("click", click_on_team);
+
+            nodes.append("text")
+                .attr("class", function(d) {
+                    return "text " + d.abbr;})
+                .attr("dx", function(d){
+                    return Scale(parseInt(d.winrate*100));})
+                .attr("dy", ".3em")
+                .attr("font-size", function(d) {
+                    return font_Size(d.rank) + "px";})
+                .style("fill", "#888888")
+                .style("font-weight", "bold")
+                .style("cursor", "default")
+                .text(function(d) {
+                    return d.abbr;});
+        });
+    });
+});
+
+function click_on_team(d) {
+    console.log(d);
+    selected_team_name = d.TEAM.split(' ')[d.TEAM.split(' ').length - 1];
+    console.log(selected_team_name);
+    var fileName = "./" + selected_team_name + ".html";
+    var newWin = open(fileName, '', 'width=1000,height=800');
+    d3.select(this).on("Click",newWin);
+
+}
+
+
+function click_on_state(d) {
+    if (active.node() == this) {
+        active.style("fill", function(d) {
+            var EASTorWEST = d.properties.EASTorWEST;
+            if (EASTorWEST) {
+                if (EASTorWEST == "East") {
+                    return "#C6E2FF";
+                } else {
+                    return "#FFB6C1";
+                }
+            } else {
+                return "#ccc";
+            }
+        });
+        stateAbb = d3.select(this).attr("class");
+        svg.selectAll(".text-" + stateAbb).remove();
+    }
+
+    active = d3.select(this).style("fill", "orange");
+
+    var bounds = path.bounds(d),
+        dx = bounds[1][0] - bounds[0][0],
+        dy = bounds[1][1] - bounds[0][1],
+        x = (bounds[0][0] + bounds[1][0]) / 2,
+        y = (bounds[0][1] + bounds[1][1]) / 2,
+        scale = 0.7 / Math.max(dx / whole_w, dy / whole_h),
+        translate = [whole_w / 2 - scale * x, whole_h / 2 - scale * y];
+
+    g.append("text")
+        .attr("class", "text-" + d.properties.postal)
+        .attr("x", x - 20)
+        .attr("y", y)
+        .attr("font-size", "10px")
+        .style("cursor", "default")
+        .text(d.properties.name);
+}
+
+
+function hover_on_node(d){
+    d3.select(this).select("circle")
+        .transition()
+        .duration(200)
+        .attr("r", function(d){
+            return 1.5 * Scale(parseInt(d.winrate*100)); })
+        .style("opacity", 1)
+        .style("stroke-width", "2px");
+
+    d3.select(this).select("text")
+        .transition()
+        .duration(200)
+        .attr("dx", function(d){
+            return 1.5 * Scale(parseInt(d.winrate*100));})
+        .style("fill", "#000000")
+        .text(function(d) {
+            return d.abbr + " (" + parseInt(d.winrate*100 )+ "%)";});
+
+    g.append("image")
+        .attr("class", d.abbr)
+        .attr("xlink:href", "logo/" + d.abbr + "_logo.svg")
+        .attr("width", image_w + "px")
+        .attr("height", image_h + "px")
+        .attr("x", projection([d.lon, d.lat])[0] + 5)
+        .attr("y", projection([d.lon, d.lat])[1] + 5);
+}
+
+
+function hover_off_node(d){
+    d3.select(this).select("circle")
+        .transition()
+        .duration(200)
+        .attr("r", function(d) {
+            return Scale(parseInt(d.winrate*100)); })
+        .style("opacity", function(d){
+            return Opacity(parseInt(d.winrate*100));})
+        .style("stroke-width", "1px");
+
+    d3.select(this).select("text")
+        .transition()
+        .duration(200)
+        .attr("dx", function(d){
+            return Scale(parseInt(d.winrate*100));})
+        .style("fill", "#888888")
+        .text(function(d) {
+            return d.abbr});
+
+    g.select("image")
+        .remove();
+    }
